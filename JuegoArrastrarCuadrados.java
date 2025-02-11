136af6fc34d654c0e5dc7807c05e550979111349
Melgar Parada Marco Antonio <nurmarcomelgar@gmail.com>
2025-02-10 23:01:01 -0400

    Delete JuegoArrastrarCuadrados.java

--- JuegoArrastrarCuadrados.java
+++
@@ -1,9 +0,0 @@
-package Practico1;
-
-import Practico1.Interface.Frame;
-
-public class JuegoArrastrarCuadrados {//Inicializar Programa en el main principal
-    public static void main (String[]args){
-        new Frame();
-    }
-}
